1ccce57c257e5552fccdeab89ba1b3434a1a4088
Ben Singer <benjaminjsinger@gmail.com>
2015-01-18 21:51:09 -0600

    updated server to work and stuff

--- server.rb
+++ server.rb
@@ -7,100 +7,69 @@ require 'limitless_led'
 puts "Starting server..."
 
 configure do 
-  
-  set :bridges, []
-  [100..255].each do |i|
-    settings.bridges.push(LimitlessLed::Bridge.new(host: "192.168.1.#{i}", port: 8899))
-  end
+  set :bridge, LimitlessLed::Bridge.new(host: "192.168.2.5", port: 8899)
 
-  def execute(command, param, group_id) 
-    Thread.new do
-      settings.bridges.each do |bridge|
-        begin
-          if group_id
-            group = bridge.group(group_id)
-            group.method(command).call param
-          else
-            bridge.method(command).call param
-          end
-        rescue
-          puts "Error running command #{command} with param #{param}"
+  def execute(command, param, group_id)
+    puts command
+    puts param
+    if group_id
+      group = settings.bridge.group(group_id)
+      3.times do  
+        if param
+          group.method(command).call param
+        else
+          group.method(command).call
+        end
+      end
+    else
+      3.times do
+        if param
+          settings.bridge.method(command).call param
+        else
+          settings.bridge.method(command).call
         end
       end
     end
   end
- 
+
+  def process(request_body)
+    begin
+      r = "{}"
+      id = nil
+      if request_body["id"] # 1..4
+        id = request_body["id"].to_i
+      end
+      if request_body["status"] # 'all_on', 'all_off', 'white', 'on', 'off'
+        execute(request_body["status"], nil, id)
+      end
+      if request_body["color"]  # 'Red' or 178
+        execute('color', request_body["color"], id)
+      end
+      if request_body["disco"]  # 'disco', 'disco_faster', 'disco_slower'
+        execute(request_body["disco"], nil, id)
+      end
+      if request_body["brightness"]  # 0-100 (translate to 2-27)
+        temp = request_body["brightness"].to_i
+        brightness = (temp * 0.25).round + 2
+        execute('brightness', brightness, id)
+      end
+    rescue
+      r = '{ error: "true"}'
+    end
+    r
+  end
 end
+
+
 
 #Set all tracked LEDs
 post '/all' do
   request.body.rewind
   request_body = JSON.parse(request.body.read) 
-  
-  settings.bridges.each do |b|
-    if request_body.status # 'all_on', 'all_off'
-      execute(request_body.status)
-    end
-    if request_body.color  # 'Red' or 178
-      execute('color', request_body.color)
-    end
-    if request_body.disco  # 'disco', 'disco_faster', 'disco_slower'
-      execute(request_body.disco)
-    end
-    if request_body.brightness  # 0-100 (translate to 2-27)
-      temp = request_body.brightness
-      brightness = (temp * 25).round + 2
-      execute('brightness', brightness)
-    end
-    if request_body.reset  # true,false
-      if request_body.reset == true
-        execute('white')  
-      end
-    end
-  end
-end
 
-# Bridges ###############################
-get '/bridge/:bridge_id' do |bridge_id|
-
-end
-
-post '/bridge/:bridge_id' do |bridge_id|
-  request.body.rewind
-  request_body = JSON.parse(request.body.read) 
-
-end
-
-put '/bridge/:bridge_id' do |bridge_id|
-  request.body.rewind
-  request_body = JSON.parse(request.body.read) 
-
-end
-
-delete '/bridge/:bridge_id' do |bridge_id|
-
-end
-
-# Groups ################################
-get '/group/:group_id' do |group_id|
-
-end
-
-post '/group/:group_id' do |group_id|
-  request.body.rewind
-  request_body = JSON.parse(request.body.read) 
-
-end
-
-put '/group/:group_id' do |group_id|
-  request.body.rewind
-  request_body = JSON.parse(request.body.read) 
-
-end
-
-delete '/group/:group_id' do |group_id|
-
-end
+  r = process(request_body)
+  puts r
+end 
 
 # Alarms ################################
 get '/alarm/:alarm_id' do |alarm_id|
